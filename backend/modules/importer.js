3b52273edafb677d697a0ef5a33d75d799aa759b
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-01-17 19:41:33 +0530

    Found new file,  backend/modules/importer.js. Committing Via Global Bash Script:git-status-automated.bat

---
+++ backend/modules/importer.js
@@ -0,0 +1,6 @@
+import { Modal, OffCanvas } from "../Entities/WindowElements.js";
+import { User } from "../Entities/User.js";
+
+window.Modal = Modal;
+window.OffCanvas = OffCanvas;
+window.User = User;
