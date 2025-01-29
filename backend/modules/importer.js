492bdc92d94b6d055ec70e4812e7b45fc8aea122
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-01-29 19:11:30 +0530

    Found modified or deleted file,  backend/modules/importer.js. Committing Via Global Bash Script:git-status-automated.bat

--- backend/modules/importer.js
+++
@@ -1,6 +0,0 @@
-import { Modal, OffCanvas } from "../Entities/WindowElements.js";
-import { User } from "../Entities/User.js";
-
-window.Modal = Modal;
-window.OffCanvas = OffCanvas;
-window.User = User;
